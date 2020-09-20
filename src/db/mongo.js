a18aaf729a3a42ee3f4ae230eb5fcd625b36ecfa
kazimhussain1 <kazimhussain0213@gmail.com>
2020-09-20 15:42:37 +0500

    updated db connection params

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -1,18 +1,23 @@
-const mongoose = require("mongoose");
-
+const mongoose = require('mongoose');
 
 mongoose
-  .connect("mongodb://localhost/salnaka_db", 
-            { useNewUrlParser: true ,
-              useUnifiedTopology: true, 
-              useFindAndModify:false,
-              useCreateIndex: true
-          })
-  .then(() => {
-    console.log("connected to db");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(process.env.MONGOOSE_CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        autoReconnect:true,
+        reconnectInterval: 10000,
+        reconnectTries: Number.MAX_VALUE,
+        bufferMaxEntries: 0
+    })
+    .then(() => {
+        console.log('connected to db');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+
 
 module.exports = mongoose.Connection;
